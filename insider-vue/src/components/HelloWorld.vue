a45e5deb0c66cb7fbc8b5caa360cfb18fb8fcfd4
chamegashi <msymsy0693wrb@gmail.com>
2021-07-30 14:38:12 +0900

    first

---
+++ insider-vue/src/components/HelloWorld.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="text-gray-800">
+    <p>test</p>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class HelloWorld extends Vue {
+  @Prop() private msg!: string;
+}
+</script>
