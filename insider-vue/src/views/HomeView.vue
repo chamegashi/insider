f4940161238bf402684067f1428e3bb499a91572
chamegashi <msymsy0693wrb@gmail.com>
2021-07-30 18:25:28 +0900

    test

--- insider-vue/src/views/HomeView.vue
+++ insider-vue/src/views/HomeView.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="bg-gray-100 h-screen">
     <p class="text-5xl p-8 text-center">テストインサイダー</p>
-    <div class="text-center">
+
+
+    <div class="text-center" v-if="status === 'login'">
       <div class="mb-4">
         <label class="block text-gray-700 text-xl font-bold mb-2">
           名前
@@ -12,21 +14,113 @@
         参加
       </button>
     </div>
+
+
+    <div class="text-center" v-if="status === 'ready'">
+      <div class="mb-4">
+        <p class="text-xl font-semibold">プレイヤー</p>
+        <div v-for="player in players" :key="player.name" class="m-2">
+          <p>{{"- " + player.name}}</p>
+        </div>
+      </div>
+      <button class="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded" @click="start">
+        次へ
+      </button>
+    </div>
+
+
+    <div class="text-center" v-if="status === 'start'">
+      <div v-if="player.role === 'people'">
+        <div class="text-center">
+          <p class="text-xl m-1 pt-5">あなたは</p>
+          <p class="text-3xl font-bold m-4">庶民</p>
+          <p class="text-xl m-1">です</p>
+          <p class="text-xl m-1">誰がインサイダーか見極めましょう。</p>
+        </div>
+      </div>
+
+      <div v-if="player.role === 'insider'">
+        <div class="text-center bg-red-700 text-white">
+          <p class="text-xl m-1 pt-5">あなたは</p>
+          <p class="text-3xl font-bold m-4">インサイダー</p>
+          <p class="text-xl m-1">です</p>
+          <p class="text-xl m-1">庶民にバレずに正解へと導きましょう。</p>
+          <p class="text-xl m-1">お題は</p>
+          <p class="text-3xl font-bold m-4 pb-5">{{answer}}</p>
+        </div>
+      </div>
+
+      <div v-if="player.role === 'master'">
+        <div class="text-center bg-gray-800 text-white">
+          <p class="text-xl m-1 pt-5">あなたは</p>
+          <p class="text-3xl font-bold m-4">マスター</p>
+          <p class="text-xl m-1">です</p>
+          <p class="text-xl m-1">誰がインサイダーか見極めましょう。</p>
+          <p class="text-xl m-1">お題は</p>
+          <p class="text-3xl font-bold m-4">{{answer}}</p>
+          <button class="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded mb-20" @click="vote">
+            頑張る
+          </button>
+        </div>
+      </div>
+    </div>
+
+
+    <div class="text-center" v-if="status === 'vote'">
+      <div class="flex justify-center m-5">
+        <div v-for="player in players" :key="player.name" class="w-1/3 m-4">
+          <label>
+            <p class="text-xl">{{player.name}}</p>
+            <input type="radio" :value="player.name" v-model="selected">
+            <p>{{selected}}</p>
+          </label>
+        </div>
+      </div>
+      <p class="text-xl">あなたの投票先</p>
+      <p class="text-3xl m-3">{{selected}}</p>
+      <button v-if="!reserve" class="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded m-20" @click="waitResult">
+        投票する
+      </button>
+      <button v-if="reserve" class="bg-gray-700 text-white font-bold py-2 px-4 rounded m-20" disabled>
+        投票完了
+      </button>
+    </div>
+
+
+    <div class="text-center" v-if="status === 'waitResult'">
+      <p class="text-3xl m-5">投票完了！</p>
+      <button class="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded m-20" @click="result">
+        結果を見る
+      </button>
+    </div>
+
     <!-- <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/> -->
   </div>
 </template>
 
 <script lang="ts">
-import HelloWorld from '../components/HelloWorld.vue';
+import Login from '../components/Login.vue';
 import { defineComponent, ref } from '@vue/composition-api';
-import router from '../router/index'
+
+interface Player {
+  name: string;
+  status: string;
+  role: string;
+  vote: string;
+}
 
 export default defineComponent({
   components: {
-    HelloWorld
+    Login
   },
   setup() {
     const name = ref<string>();
+    const players = ref<Player[]>();
+    const player = ref<Player>();
+    const answer = ref<string>();
+    const selected = ref<string>();
+    const reserve = ref<boolean>();
+    const status = ref<string>("login");
 
     const ws = new WebSocket('ws://localhost:12345');
 
@@ -39,20 +133,95 @@ export default defineComponent({
         ws.send(JSON.stringify(data));
       }
     }
-    
+
+    const start = () => {
+      const data = {
+        status: 'ready'
+      }
+      ws.send(JSON.stringify(data));
+    }
+
+    const vote = () => {
+      const data = {
+        status: 'start'
+      }
+      ws.send(JSON.stringify(data));
+    }
+
+    const waitResult = () => {
+      const data = {
+        status: 'vote',
+        vote: selected.value
+      }
+      ws.send(JSON.stringify(data));
+    }
+
+    const result = () => {
+      const data = {
+        status: 'waitResult',
+      }
+      ws.send(JSON.stringify(data));
+    }
+
     ws.onmessage = (event) => {
-      console.log(event.data)
       const obj = JSON.parse(event.data)
-      console.log(obj)
       if(obj['next_status'] === "ready"){
-        router.push('/wait')
+        status.value = 'ready';
+        players.value = obj.data
+      }
+
+      if(obj['next_status'] === "start"){
+        status.value = 'start';
+        player.value = obj.data;
+        answer.value = obj.answer;
+      }
+
+      if(obj['next_status'] === "vote"){
+        status.value = 'vote';
+        players.value = obj.data;
+        reserve.value = obj.reserve;
+      }
+
+      if(obj['next_status'] === "waitResult"){
+        status.value = 'waitResult';
       }
     }
 
+    players.value = [
+      {
+        name: "name1",
+        status: "string",
+        role: "string",
+        vote: "string",
+      },
+      {
+        name: "name2",
+        status: "string",
+        role: "string",
+        vote: "string",
+      },
+      {
+        name: "name3",
+        status: "string",
+        role: "string",
+        vote: "string",
+      },
+    ]
+
     return {
       name,
+      players,
+      player,
+      answer,
+      selected,
+      reserve,
       join,
-      ws
+      start,
+      vote,
+      waitResult,
+      result,
+      ws,
+      status
     }
   },
 });
--- webserver.py
+++ webserver.py
@@ -1,3 +1,4 @@
+import random
 import json
 import logging
 from websocket_server import WebsocketServer
@@ -9,10 +10,11 @@ handler.setFormatter(logging.Formatter(' %(module)s -  %(asctime)s - %(levelname
 logger.addHandler(handler)
 
 clients = []
+answer = ""
 
 # Callback functions
 
-def get_player_data():
+def get_players_data():
     ret = []
     for client in clients:
         if(client['name']):
@@ -24,6 +26,27 @@ def get_player_data():
             })
 
     return ret
+
+def get_player_data(index):
+    ret = {}
+    if(clients[index]['name']):
+        ret = {
+            'name': clients[index]['name'],
+            'status': clients[index]['status'],
+            'role': clients[index]['role'],
+            'voted': clients[index]['voted'],
+        }
+
+    return ret
+
+def check_all_voted():
+    result = True
+    clients_data = get_players_data()
+    for client in clients_data:
+        if(client['voted'] == 'none'):
+            result = False
+    
+    return result
 
 def new_client(client, server):
   logger.info('New client {}:{} has joined.'.format(client['address'][0], client['address'][1]))
@@ -37,24 +60,91 @@ def client_left(client, server):
 
  
 def message_received(client, server, message):
-  logger.info('Message "{}" has been received from {}:{}'.format(message, client['address'][0], client['address'][1]))
-  mes = json.loads(message)
-  if(mes['status'] == 'login'):
-    index = next((index for (index, d) in enumerate(clients) if d['id'] == client['id']), None)
-    clients[index]['name'] = mes['name']
-    clients[index]['status'] = 'ready'
-    clients[index]['role'] = 'none'
-    clients[index]['voted'] = 0
-    ret = {
-        'next_status' : 'ready',
-        'data': get_player_data(),
-    }
-    server.send_message(client, json.dumps(ret))
+    logger.info('Message "{}" has been received from {}:{}'.format(message, client['address'][0], client['address'][1]))
+    mes = json.loads(message)
 
-  print(client)
+    if(mes['status'] == 'login'):
+        index = next((index for (index, d) in enumerate(clients) if d['id'] == client['id']), None)
+        clients[index]['name'] = mes['name']
+        clients[index]['status'] = 'ready'
+        clients[index]['role'] = 'none'
+        clients[index]['voted'] = 'none'
+        ret = {
+            'next_status' : 'ready',
+            'data': get_players_data(),
+        }
+        for c in clients:
+            server.send_message(c, json.dumps(ret))
+    
+    elif (mes['status'] == 'ready'):
+        role_list = []
+        for i in range(len(clients)):
+            if(i == 0):
+                role_list.append('master')
+            elif(i == 1):
+                role_list.append('insider')
+            else:
+                role_list.append('people')
+        
+        random.shuffle(role_list)
+        answer = "布団"
 
-  # server.send_message(client, reply_message)
-  # logger.info('Message "{}" has been sent to {}:{}'.format(reply_message, client['address'][0], client['address'][1]))
+        for i in range(len(clients)):
+            clients[i]['role'] = role_list[i]
+            ret = {
+                'next_status' : 'start',
+                'data': get_player_data(i),
+                'answer': answer
+            }
+            server.send_message(clients[i], json.dumps(ret))
+    
+    elif (mes['status'] == 'start'):
+        ret = {
+            'next_status': 'vote',
+            'data': get_players_data(),
+            'reserve': False
+        }
+        for i in range(len(clients)):
+            server.send_message(clients[i], json.dumps(ret))
+
+    elif (mes['status'] == 'vote'):
+        index = next((index for (index, d) in enumerate(clients) if d['id'] == client['id']), None)
+        clients[index]['voted'] = mes['vote']
+
+        if(check_all_voted()):
+            ret = {
+                'next_status': 'waitResult',
+            }
+            for i in range(len(clients)):
+                server.send_message(clients[i], json.dumps(ret))
+        
+        else:
+            ret = {
+                'next_status': 'vote',
+                'data': get_players_data(),
+                'reserve': True
+            }
+            server.send_message(client, json.dumps(ret))
+
+    elif (mes['status'] == 'waitResult'):
+
+        if(check_all_voted()):
+            ret = {
+                'next_status': 'waitResult',
+            }
+            for i in range(len(clients)):
+                server.send_message(clients[i], json.dumps(ret))
+        
+        else:
+            ret = {
+                'next_status': 'vote',
+                'data': get_players_data(),
+                'reserve': True
+            }
+            server.send_message(client, json.dumps(ret))
+
+
+
 
 # Main
 if __name__ == "__main__":
